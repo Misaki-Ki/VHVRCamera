bb31cfc8c357b5bdb9d7c731fa760e409b1243bf
Misaki <Misaki@D-PC>
2022-08-27 14:33:56 -0500

    Add project files.

---
+++ Component/CameraComponent.cs
@@ -0,0 +1,138 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+using System.Reflection;
+
+
+namespace VHVRCamera
+{
+
+    public class CameraComponent : MonoBehaviour
+    {
+        GameObject followerCameraObject;
+        Transform targetTransform;
+        Camera followCam;
+        Camera targetCam;
+
+        private Vector3 velocity = Vector3.zero;
+
+
+        void Start()
+        {
+            followerCameraObject = this.gameObject;
+            targetTransform = new GameObject().transform;
+
+            followCam = GetComponent<Camera>();
+            followCam.stereoTargetEye = StereoTargetEyeMask.None;
+            followCam.fieldOfView = 100f;
+            followCam.targetDisplay = 0;
+            followCam.depth = 500;
+            followCam.nearClipPlane = 0.01f;
+            followCam.enabled = false;
+
+            foreach (Camera camera in FindObjectsOfType<Camera>())
+            {
+                Debug.Log(camera.name);
+                if (camera.name == "Main Camera")
+                { //targetTransform = camera.transform;
+                    Debug.Log("Main Camera Found");
+                    followCam.cullingMask = camera.cullingMask;
+                }
+
+            }
+
+        }
+
+
+
+
+
+        void Update()
+        {
+
+            if (Input.GetKeyDown("h"))
+            {
+                Debug.Log("Key H Press");
+                followCam.enabled = !followCam.enabled;
+
+                if (followCam.enabled == true)
+                {
+                    Debug.Log("Camera is Enabled.");
+                    Debug.Log(followerCameraObject);
+
+                    if (Player.m_localPlayer != null)
+                    {
+                        transform.position = Player.m_localPlayer.transform.position;
+                    }
+                }
+
+                else
+                {
+                    Debug.Log("Camera is Disabled.");
+                }
+            }
+
+
+        }
+
+
+        void tryassignTarget()
+        {
+            if (Player.m_localPlayer != null)
+            {
+                Debug.Log("Player Head Point: " + Player.m_localPlayer.GetHeadPoint());
+                targetTransform = Player.m_localPlayer.m_animator.GetBoneTransform(HumanBodyBones.Head);
+            }
+
+        }
+
+        void LateUpdate()
+        {
+            Vector3 targetPosition;
+            if (targetTransform == null)
+            {
+                if (Player.m_localPlayer != null)
+                {
+                    targetTransform = Player.m_localPlayer.m_animator.GetBoneTransform(HumanBodyBones.Head);
+                }
+
+            }
+
+            else if (followCam.enabled)
+            {
+
+                Debug.Log("Spec Position: " + transform.position);
+                Debug.Log("Target position : " + targetTransform.position);
+
+                
+                
+
+                Vector3 offset = new Vector3(0,0,-2);
+                targetPosition = targetTransform.position + offset;
+                transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.6f);
+                transform.LookAt(targetTransform);
+
+                //  transform.position = Vector3.Lerp(transform.position, targetTransform.position, 0.9f * Time.unscaledDeltaTime);
+                // transform.rotation = Quaternion.RotateTowards(transform.rotation, targetTransform.rotation, 50f * Time.unscaledDeltaTime);
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using UnityEngine;
+using System;
+
+namespace VHVRCamera
+{
+
+    [BepInPlugin("org.bepinex.plugins.VHVRCamera", "Valheim VR Camera", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Debug.Log("Camera Mod is Awake");
+
+
+            // This section is borrowed from: <https://github.com/octoberU/SuperHotVRSpectatorCamera/blob/main/SpectatorCameraMod.cs>
+
+            var CamObject = new GameObject("FollowCamera", new Type[]
+            {
+                typeof(CameraComponent),
+                typeof(Camera)
+            }
+            );
+            CamObject.GetComponent<CameraComponent>().enabled = true;
+
+            CamObject.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            GameObject.DontDestroyOnLoad(CamObject);
+
+
+        }
+
+       
+
+
+
+
+
+
+    }
+
+
+
+    }
+
+
+
+
+
